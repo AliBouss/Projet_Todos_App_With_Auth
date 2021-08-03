375a1e9516afea1f9a005f11d0e83f6450e4da7b
Alibouss <bouss58@hotmail.com>
2021-08-03 16:18:23 +0200

    create views createview, detailview, listview

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from todolist.views import TaskList, TaskDetail
+from todolist.views import TaskDetail, TaskCreate, task_list
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name="details")
+    path('', task_list, name="tasks"),
+    path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
+    path('task-create/', TaskCreate.as_view(), name="task-create")
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,19 +1,46 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, DetailView
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, DetailView, CreateView
 from todolist.models import Task
 
 
-class TaskList(ListView):
-    model = Task
-    template_name = "base/task_list.html"
-    get_context_name = "tasks"
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'base/task_list.html', {'tasks': tasks})
 
 
 class TaskDetail(DetailView):
     model = Task
     template_name = "base/task_detail.html"
     get_context_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'base/task_form.html'
+
+
+"""
+fonction based view
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'base/task_list.html', {'tasks': tasks})
+    
+def task_detail(request, pk):
+    tasks = Task.objects.get(id=pk)
+
+    return render(request, 'base/task_detail.html', {'tasks': tasks})
+
+class based view
+
+class TaskList(ListView):
+    model = Task
+    template_name = "base/task_list.html"
+    get_context_name = "tasks"
+"""
 
 
