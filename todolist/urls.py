b3bbac32d83fd442ee62f81a29bba8801ed70da1
Alibouss <bouss58@hotmail.com>
2021-08-05 15:16:18 +0200

    User specific data, User registration, search fonctionnality

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import TaskList, HomeView, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, HomeView, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name="task-delete"),
     path('login/', CustomLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('register/', RegisterPage.as_view(), name='register')
 
 ]
